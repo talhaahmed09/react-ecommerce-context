32aacc748bb2802e04f4fbe93f4c65f7b4ee4263
talhaahmed09 <endhogaya3@gmail.com>
2022-06-27 18:09:50 +0500

    style: login form ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { Button, Form, Input } from "antd";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth/AuthProvider";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 
 const Login = () => {
   const {logIn} = useAuth();
@@ -26,22 +27,24 @@ const Login = () => {
   };
 
   return (
-    <div className="container  ">
-      <h1 className="p-3  text-center">Login</h1>
-
-     <div className="d-flex flex-wrap justify-content-center align-items-center">
+    <div className="container ">
+        <div className="d-flex flex-wrap align-items-center justify-content-center">
+      <div className="p-3 h1 text-center">Login</div>
+     
+     <div className="container">
+       <div className="d-flex justify-content-center">
+     
       <Form
         name="basic"
         layout="horizontal"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 12 }}
+        className="w-50"
         initialValues={{ remember: true }}
         onFinish={handleForm}
         onFinishFailed={onFinishFailed}
         autoComplete="off"
+
       >
         <Form.Item
-          label="Username"
           name="username"
           rules={[
             {
@@ -50,11 +53,10 @@ const Login = () => {
             },
           ]}
         >
-          <Input />
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
         </Form.Item>
 
         <Form.Item
-          label="Password"
           name="password"
           rules={[
             {
@@ -63,15 +65,20 @@ const Login = () => {
             },
           ]}
         >
-          <Input.Password />
+          <Input.Password   prefix={<LockOutlined className="site-form-item-icon" />}
+          placeholder="Password"/>
         </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
+        <Form.Item  >
+          <Button type="primary" htmlType="submit" className="w-50">
             Submit
           </Button>
+          <div className="my-4 " style={{textAlign:'left'}}>New member? <Link to="/register">Register</Link> here</div>
         </Form.Item>
+  
       </Form>
+      </div>
+      </div>
       </div>
     </div>
   );
